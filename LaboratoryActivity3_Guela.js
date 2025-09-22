bc3b631a3e2735f3024950172129ccd3bc02e705
Ashtyn Sam <ashtynsamhernandez.guela24@bicol-u.edu.ph>
2025-09-22 20:19:35 +0800

    Code update for better comprehension of instructions.

--- LaboratoryActivity3_Guela.js
+++ LaboratoryActivity3_Guela.js
@@ -22,32 +22,34 @@ function calculateGrade(score) {
 }
  
 function showStars(rows) {
-    console.log("#2: ")
-    for (let i = 1; i <= rows; i++) {
-        const spaces = ' '.repeat(rows - i);
-        const stars = '*'.repeat(2 * i - 1);
-        console.log(spaces + stars + spaces);
-    }
-} 
+    rows += 2;
+    for (let row = 1; row <= rows; row++) {
+        let pattern = '';
+            for (let i = 0; i < row; i++)
+                pattern += '*'; 
+    console.log(pattern);
+  }
+}
+
 
 
 function isPrime(number) {
   for (let factor = 2; factor < number; factor++) 
     if (number % factor === 0) 
-      return 'Not Prime'; 
+      return 'not prime'; 
 
-  return 'Prime'; 
+  return 'prime'; 
 }
 
 function multiplicationTable(number) {
 for (let i = 0; i <= 10; i++)
-console.log(`${i * number}`);
+console.log(`${number} * ${i} = ${i * number}`);
 }
 
 
 console.log(`\n\n||Base Number value is ${base_number}|| ||Student ID is 2024-01-05836||\n`);
 console.log(`#1: The grade calculated is ${calculateGrade(base_number)}\n`);
 showStars(base_number);
-console.log("\n#3: "+ isPrime(base_number)); 
+console.log("\n#3: "+ base_number, "is " + isPrime(base_number)); 
 console.log(`\n#4: Multiplication Table of ${base_number}:`);
 multiplicationTable(base_number);
